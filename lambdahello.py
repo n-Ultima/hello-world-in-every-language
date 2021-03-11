53b8cd44371d7b2b5aae231efc7d4b7afd4c283b
n-Ultima <78455052+n-Ultima@users.noreply.github.com>
2021-03-11 11:36:51 -0600

    Create lambdahello.py
    This prints, "Hello, World!".

---
+++ lambdahello.py
@@ -0,0 +1,18 @@
+(
+  (_:=lambda _:(_ := _.__add__(__name__.__len__().__bool__().__int__())),
+  
+  (__import__("builtins").print(
+    chr(__:=[_(x) for x in range(ord(chr(72)).__int__())][-1]).__add__(
+      chr(__:=[_(x) for x in range(ord(chr(101)).__int__())][-1]).__add__(
+        chr(__:=[_(x) for x in range(ord(chr(108)).__int__())][-1]).__add__(
+          chr(__:=[_(x) for x in range(ord(chr(108)).__int__())][-1]).__add__(
+            chr(__:=[_(x) for x in range(ord(chr(111)).__int__())][-1]).__add__(
+              chr(__:=[_(x) for x in range(ord(chr(44)).__int__())][-1]).__add__(
+                chr(__:=[_(x) for x in range(ord(chr(32)).__int__())][-1]).__add__(
+                  chr(__:=[_(x) for x in range(ord(chr(87)).__int__())][-1]).__add__(
+                    chr(__:=[_(x) for x in range(ord(chr(111)).__int__())][-1]).__add__(
+                      chr(__:=[_(x) for x in range(ord(chr(114)).__int__())][-1]).__add__(
+                        chr(__:=[_(x) for x in range(ord(chr(108)).__int__())][-1]).__add__(
+                          chr(__:=[_(x) for x in range(ord(chr(100)).__int__())][-1]).__add__(
+                            chr(__:=[_(x) for x in range(ord(chr(33)).__int__())][-1])
+))))))))))))))))
